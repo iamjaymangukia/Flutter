29b7f2559c502482acce0080652545e871797e17
iamjaymangukia <80069986+iamjaymangukia@users.noreply.github.com>
2022-10-04 00:55:18 +0530

    lab11 added

---
+++ lab11/lib/main.dart
@@ -0,0 +1,18 @@
+//Lab :- 11
+
+import 'package:flutter/material.dart';
+import 'package:a4_lab_11/pages/home.dart';
+import 'package:a4_lab_11/pages/choose_location.dart';
+import 'package:a4_lab_11/pages/loading.dart';
+
+void main() => runApp(MaterialApp(
+// home: Home(),
+// instead of making home: property to make any page to initialize at beginning...
+// we can use following code ....
+// initialRoute: '/home',
+        initialRoute: '/',
+        routes: {
+          '/': (context) => Loading(),
+          '/home': (context) => Home(),
+          '/location': (context) => ChooseLocation(),
+        }));
